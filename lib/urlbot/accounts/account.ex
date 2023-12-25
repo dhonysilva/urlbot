33f103cf27c50abd0baa4c4bdec55329b3a4bb75
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-12-25 14:51:33 -0300

    Account context

---
+++ lib/urlbot/accounts/account.ex
@@ -0,0 +1,19 @@
+defmodule Urlbot.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
